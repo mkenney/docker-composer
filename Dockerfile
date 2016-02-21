ae379fcb2e97cfb01e85791ba7ce466f419fe7dc
Michael Kenney <michael.kenney@returnpath.net>
2016-02-20 17:39:26 -0700

    Minor updates / cleanup

--- Dockerfile
+++ Dockerfile
@@ -39,9 +39,3 @@ ENV COMPOSER_VERSION master
 
 # Install Composer
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-# Display version information
-RUN composer --version
-
-CMD ["-"]
-ENTRYPOINT ["composer", "--ansi"]
--- composer.sh
+++ composer.sh
@@ -4,6 +4,5 @@ container_name=composer-temp
 if [[ $(docker ps -a | grep $container_name) ]]; then
     docker rm -f $container_name > /dev/null
 fi
-docker run --name=$container_name -v $(pwd):/app:rw mkenney/composer composer $1
-docker rm -f $container_name > /dev/null
+docker run --rm --name=$container_name -v $(pwd):/app:rw mkenney/composer composer $@
 
